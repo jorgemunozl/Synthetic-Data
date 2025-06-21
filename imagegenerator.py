567a6864f693d2b1ea04886731c409ac401685c2
jorgemunozl <alvaro18ML@gmail.com>
2025-06-20 20:08:49 -0500

    last

--- imagegenerator.py
+++ imagegenerator.py
@@ -91,12 +91,8 @@ def createJson(info: dict, namefile: str) -> bool:
 
 listTool = [createFile,createImage]
 
-firstMessage = 
-
-"""
-I want that you create a prompt of any image that you want in the topic of science, and using that prompt to create a image and save it,
+firstMessage = """I want that you create a prompt of any image that you want in the topic of science, and using that prompt to create a image and save it,
 also create a .txt where you save that prompt.
-
 """
 
 skill0 , skill1 = "creative", "tidy"
