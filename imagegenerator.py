891aff83b70d066959737e8e137bed5a579070b4
jorgemunozl <alvaro18ML@gmail.com>
2025-06-20 20:00:48 -0500

    clean

--- imagegenerator.py
+++ imagegenerator.py
@@ -15,23 +15,7 @@ import json
 # chain (runnable) => llm + output parser. + prompt =>
 # tools => function calling => brinda las definiciones => una tool es una function que lo que hace es decirle al modelo que datos ingresa y que datos exactos quiere que retorne,
 
-"""
-{
-  "flowId": "103944, int
-  "name": "process_coffee", str
-  "description": "Process to prepare and drink coffee", str
-  "version": "1.0", 
-  "startNode": "n1",
-  "nodes": [
-    { "id": "n1", "type": "start", "label": "Inicio" },
-    { "id": "n2", "type": "decision", "label": "¿Quieres café?" },
-  ],
-  "edges": [ id=[(e1,n1,n2,condition)]
-    { "id": "e1", "from": "n1", "to": "n2", "condition": null },
-    { "id": "e2", "from": "n2", "to": "n10", "condition": "NO" },
-  ]
-}
-"""
+
 
 def createImage(prompt: str, name: str, size: str = "1024x1024", quality: str = "standard") -> bool:
     """
@@ -108,8 +92,8 @@ def createJson(info: dict, namefile: str) -> bool:
 listTool = [createFile,createImage]
 
 firstMessage = 
-"""
 
+"""
 I want that you create a prompt of any image that you want in the topic of science, and using that prompt to create a image and save it,
 also create a .txt where you save that prompt.
 
@@ -187,3 +171,22 @@ initial = {
 messages = graph.invoke(initial)
 for m in messages['messages']:
     m.pretty_print()
+
+
+"""
+{
+  "flowId": "103944, int
+  "name": "process_coffee", str
+  "description": "Process to prepare and drink coffee", str
+  "version": "1.0", 
+  "startNode": "n1",
+  "nodes": [
+    { "id": "n1", "type": "start", "label": "Inicio" },
+    { "id": "n2", "type": "decision", "label": "¿Quieres café?" },
+  ],
+  "edges": [ id=[(e1,n1,n2,condition)]
+    { "id": "e1", "from": "n1", "to": "n2", "condition": null },
+    { "id": "e2", "from": "n2", "to": "n10", "condition": "NO" },
+  ]
+}
+"""
